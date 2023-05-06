d1928a608c600248df09fce7e5ebd5b380f8682c
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-06 11:49:09 -0300

    Updates

--- script.js
+++ script.js
@@ -4,21 +4,27 @@ const climaBox = document.querySelector('.clima-box');
 const extrasBox = document.querySelector('.extras-box');
 
 // Cria elementos child
+const locationIcon = document.createElement('i');
+locationIcon.classList.add('location-icon', 'fa-solid', 'fa-location-dot');
+
 const pais = document.createElement('input');
 pais.classList.add('pais');
 pais.setAttribute('type', 'text');
 pais.setAttribute('placeholder', 'Insira sua localização');
 
-
-const pesquisa = document.createElement('div');
-pesquisa.classList.add('pesquisa');
-pesquisa.innerHTML = "ICON";
+const pesquisa = document.createElement('span');
+pesquisa.classList.add('pesquisa', 'material-symbols-outlined');
+pesquisa.innerHTML = "search";
 
 const ceuFigura = document.createElement('div');
 ceuFigura.classList.add('ceu-figura');
-ceuFigura.innerHTML = "CÉU.IMG";
+
 
 // Insere elementos child nos elementos parent
+locationBox.appendChild(locationIcon);
 locationBox.appendChild(pais);
 locationBox.appendChild(pesquisa);
 locationBox.appendChild(ceuFigura);
+
+// Lógica de digitar no campo
+
